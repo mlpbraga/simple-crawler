d36a359e851bc6a153eeb19b2fc62e86a70ae885
Luisa Braga <mlpb@icomp.ufam.edu.br>
2022-04-29 13:17:44 -0400

    feat: target parameter to main file

--- __main__.py
+++ __main__.py
@@ -1,14 +1,16 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Crawller.')
+parser.add_argument('target', type=str, help='target URL to retrieve information')
 parser.add_argument('--print', default=True,
-                    action='store_true', help='Print crawller results.')
+                    action='store_true', help='print crawller results')
 parser.add_argument('--save_csv', default=False,
-                    action='store_true', help='Save crawller results to csv file.')
+                    action='store_true', help='save crawller results to csv file')
 parser.add_argument('--save_json', default=False,
-                    action='store_true', help='Save crawller results to json file.')
+                    action='store_true', help='save crawller results to json file')
 args = parser.parse_args()
 
+print(args.target)
 if args.print:
     print('Print')
 if args.save_csv:
