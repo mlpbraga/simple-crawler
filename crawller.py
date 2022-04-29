552f7671f0d8db4b64b5dfadb85e2b722889df2e
Luisa Braga <mlpb@icomp.ufam.edu.br>
2022-04-29 15:14:00 -0400

    refactor: add exception handler to main execution

--- crawller.py
+++ crawller.py
@@ -47,11 +47,14 @@ parser.add_argument('--save_json', default=False,
                     action='store_true', help='save crawller results to json file')
 args = parser.parse_args()
 
-results_path = f'{RESULTS_PATH}{url_mapper[args.target]}_{ts}'
-results = crawller(args.target)
-if args.print:
-    print(results)
-if args.save_csv:
-    results.to_csv(f'{results_path}.csv', index=False)
-if args.save_json:
-    results.to_json(f'{results_path}.json')
+try:
+    results_path = f'{RESULTS_PATH}{url_mapper[args.target]}_{ts}'
+    results = crawller(args.target)
+    if args.print:
+        print(results)
+    if args.save_csv:
+        results.to_csv(f'{results_path}.csv', index=False)
+    if args.save_json:
+        results.to_json(f'{results_path}.json')
+except Exception as e:
+    print(f'ERROR: Failed retrieve information from {args.target}. Please, use a valid URL.')
